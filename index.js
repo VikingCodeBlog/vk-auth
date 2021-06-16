e4fb4c7422a3ce09c1f895da3a3309901126d526
Código Vikingo <vikingcodeblog@gmail.com>
2021-06-16 19:55:13 +0200

    feat(package): add eslint and cross-env

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const localLogin = require('./src/localLogin')
 const createUser = require('./src/createUser')
 app.use(express.json());
 
-if (!process.env.production) {
+if (!process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 //Init mongo database
-const db = require('vkmongo');
+require('vkmongo');
 
 app.listen(process.env.port || 3000, () => {
     console.log('vkAuth up')
